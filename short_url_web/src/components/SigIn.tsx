4a6027426431e9c36b2536c98c82770c9a34e62b
Renan Pereira Loureiro <renanpl47@gmail.com>
2022-04-11 09:13:32 -0300

    feat: auth context

---
+++ short_url_web/src/components/SigIn.tsx
@@ -0,0 +1,53 @@
+import { Button, Stack } from "@chakra-ui/react";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useForm } from "react-hook-form";
+import * as Yup from "yup";
+import { Input } from "./Form/Input";
+import { InputPassword } from "./Form/InputPassword";
+interface FormData {
+  email: string;
+  password: string;
+}
+
+interface SignInProps {
+  handleSignIn: (data: FormData) => Promise<void>;
+}
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .email("E-mail inválido!")
+    .required("E-mail é obrigatório!"),
+  password: Yup.string().required("Senha é obrigatória!"),
+});
+
+export const SignIn = ({ handleSignIn }: SignInProps) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+  return (
+    <Stack
+      flex="1"
+      spacing={4}
+      as="form"
+      maxW="96"
+      align="center"
+      onSubmit={handleSubmit(handleSignIn)}
+    >
+      <Input placeholder="E-mail" {...register("email")} error={errors.email} />
+
+      <InputPassword
+        placeholder="Senha"
+        {...register("password")}
+        error={errors.password}
+      />
+
+      <Button title="Entrar" colorScheme="purple" type="submit" width="100%">
+        Entrar
+      </Button>
+    </Stack>
+  );
+};
